f0c5e34ef5314b275da60b67dd5c86f1a03e5826
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-02-26 09:53:44 -0800

    Completed 3.11

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 morgan.token('body', (request, response) => JSON.stringify(request.body));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
+app.use(express.static('dist'));
 
 let phonebook = [
   {
