1df96370ab1bd86679d6b982fdad99c66ef6563c
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-02-27 11:36:23 -0800

    Completed 3.17

--- index.js
+++ index.js
@@ -14,28 +14,23 @@ app.use(
 );
 app.use(express.static('dist'));
 
-let phonebook = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+//This needs to be the last loaded middleware
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
@@ -52,16 +47,24 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const person = Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  const person = Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
-  const person = Person.findByIdAndDelete(request.params.id).then((person) => {
-    response.status(204).end();
-  });
+  const person = Person.findByIdAndDelete(request.params.id)
+    .then((person) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons/', (request, response) => {
@@ -72,9 +75,12 @@ app.post('/api/persons/', (request, response) => {
     number,
   });
 
-  person.save().then(() => {
-    response.json(person);
-  });
+  person
+    .save()
+    .then(() => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT;
