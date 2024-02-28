d746a8b2d81dbe5ddb046a44a9fcfa14a02f45f6
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-02-28 09:31:04 -0800

    Completed 3.19

--- models/person.js
+++ models/person.js
@@ -16,7 +16,11 @@ mongoose
   });
 
 const phonebookSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
   number: String,
 });
 
