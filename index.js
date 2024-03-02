2417db610855fcc9bdd8e41e6ce833d71b0df679
Andrew Taurianen <andrew.taurianen@gmail.com>
2024-03-01 22:42:22 -0800

    Completed 3.21

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
-  const person = Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then((person) => {
       if (person) {
         response.json(person);
